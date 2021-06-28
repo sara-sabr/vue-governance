8548c88a4818698e3277b496598bc34b3c4eafa7
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-06-28 15:04:46 -0400

    Add committees

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
-    <Committees msg="Test" :rates="rates" />
+    <Committees :rates="rates" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Committees from "@/components/Committees.vue"; // @ is an alias to /src
-import { Rate } from "@/store/types";
+import { Committee, Rate } from "@/store/types";
 
 @Component({
   components: {
@@ -15,7 +15,7 @@ import { Rate } from "@/store/types";
   },
 })
 export default class Home extends Vue {
-  // committees: Committee = require("@/assets/data/committees.yaml");
   rates: Rate[] = require("@/assets/data/rates.json");
+  committees: Committee[] = require("@/assets/data/committees.json");
 }
 </script>
