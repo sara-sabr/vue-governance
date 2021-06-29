44448b496fd186f3fd9936cedc7de1c4fe60baf1
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-06-29 14:41:23 -0400

    Add committee details

--- src/components/CommitteeDetails.vue
+++ src/components/CommitteeDetails.vue
@@ -10,35 +10,34 @@
       <tbody>
         <tr>
           <td>{{ $t("committeeTable.membersCount") }}</td>
-          <td></td>
+          <td>{{ membersCount() }}</td>
         </tr>
         <tr>
           <td>{{ $t("committeeTable.standingPartipantsCount") }}</td>
-          <td></td>
+          <td>{{ committee.standingParticipants.length }}</td>
         </tr>
         <tr>
           <td>{{ $t("committeeTable.totalParticipants") }}</td>
-          <td></td>
+          <td>{{ allAttendantsCount() }}</td>
         </tr>
         <tr>
           <td>{{ $t("committeeTable.meetingDuration") }}</td>
-          <td></td>
+          <td>{{ committee.meetings.duration }}</td>
         </tr>
         <tr>
           <td>{{ $t("committeeTable.meetingCost") }}</td>
-          <td></td>
+          <td>{{ calculateMeetingCost().toFixed(2) }}</td>
         </tr>
         <tr>
           <td>{{ $t("committeeTable.meetingAnnualCost") }}</td>
-          <td></td>
+          <td>{{ calculateYearlyMeetingCost().toFixed(2) }}</td>
         </tr>
       </tbody>
     </table>
-    <p>{{ committee }}</p>
   </div>
 </template>
 <script lang="ts">
-import { Committee } from "@/store/types";
+import { Committee, Position, Rate } from "@/store/types";
 import Vue, { PropType } from "vue";
 import Component from "vue-class-component";
 
@@ -52,6 +51,13 @@ import Component from "vue-class-component";
       type: String,
       required: true,
     },
+    branchPositions: {
+      type: Array as PropType<Position[]>,
+      required: true,
+    },
+    rates: {
+      type: Array as PropType<Rate[]>,
+    },
   },
 })
 export default class CommitteeDetails extends Vue {
@@ -64,8 +70,83 @@ export default class CommitteeDetails extends Vue {
   }
 
   allAttendantsCount(): number {
-    const count = this.membersCount() + this.$props.committee.members.length;
+    const count =
+      this.membersCount() + this.$props.committee.standingParticipants.length;
     return count;
+  }
+
+  getCommitteePositionIds(): string[] {
+    return [
+      ...this.$props.committee.chairs,
+      ...this.$props.committee.viceChairs,
+      ...this.$props.committee.members,
+      ...this.$props.committee.standingParticipants,
+    ];
+  }
+
+  getClassLevel(positionId: string): string {
+    const positionIndex = this.$props.branchPositions.findIndex(
+      (position: Position) => {
+        return position.id === positionId;
+      }
+    );
+    if (positionIndex === -1) {
+      return "-";
+    }
+    const classLevel =
+      this.$props.branchPositions[positionIndex].classification +
+      "-0" +
+      this.$props.branchPositions[positionIndex].level;
+
+    return classLevel;
+  }
+
+  calculateHourlyRate(positionId: string): number {
+    const classLevel = this.getClassLevel(positionId);
+    if (classLevel === "-") {
+      return 0;
+    }
+    const rateIndex = this.$props.rates.findIndex((rate: Rate) => {
+      return rate.label === classLevel;
+    });
+
+    if (rateIndex == -1) return 0;
+
+    const hourlyRate = Number(this.$props.rates[rateIndex].min);
+    return hourlyRate / 1950;
+  }
+
+  calculateMeetingCost(): number {
+    const positionIds = this.getCommitteePositionIds();
+    let totalCost = 0;
+    positionIds.forEach((id) => {
+      totalCost += Number(this.calculateHourlyRate(id));
+    });
+    return totalCost;
+  }
+
+  calculateYearlyMeetingCost(): number {
+    let totalOccurences = 0;
+    switch (this.$props.committee.meetings.recurrence) {
+      case "weekly":
+        totalOccurences = 52;
+        break;
+      case "bi-weekly":
+        totalOccurences = 52 / 2;
+        break;
+      case "montly":
+        totalOccurences = 12;
+        break;
+      case "bi-monthly":
+        totalOccurences = 6;
+        break;
+      case "quarterly":
+        totalOccurences = 3;
+        break;
+      default:
+        totalOccurences = 12;
+    }
+    return this.calculateMeetingCost() * totalOccurences;
   }
 }
 </script>
