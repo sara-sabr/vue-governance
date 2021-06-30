14e9e6349fe9737ae17eb266dacf4f39ff4ea89c
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-06-29 21:48:26 -0400

    Add branch positions view

---
+++ src/views/Committees.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <CommitteeList
+      :rates="rates"
+      :committees="committees"
+      :lang="lang"
+      :branch-positions="branchPositions"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import CommitteeList from "@/components/CommitteeList.vue"; // @ is an alias to /src
+import { Committee, Position, Rate } from "@/store/types";
+
+@Component({
+  components: {
+    CommitteeList,
+  },
+})
+export default class Home extends Vue {
+  rates: Rate[] = require("@/assets/data/rates.json");
+  committees: Committee[] = require("@/assets/data/committees.json");
+  branchPositions: Position[] = require("@/assets/data/branchPositions.json");
+  lang = this.$i18n.locale;
+}
+</script>
