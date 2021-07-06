63e3f6fc489fe5434c2477853c29f36d324c76aa
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-07-06 14:57:28 -0400

    Add paths

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,33 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Committees from "../views/Committees.vue";
+import Home from "../views/Home.vue";
+import BranchPositions from "../views/BranchPositions.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/committees",
     name: "Committees",
     component: Committees,
   },
   {
     path: "/branch-positions",
     name: "Branch Positions",
+    component: BranchPositions,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/BranchPositions.vue"),
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/BranchPositions.vue"),
   },
+  { path: "*", name: "notFound", component: Home },
 ];
 
 const router = new VueRouter({
