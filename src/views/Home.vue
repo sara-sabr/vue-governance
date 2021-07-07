f8d049789a51e62c015a5b7437ed95178f2e63e9
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-07-07 17:23:50 -0400

    Add home view

---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">{{ $t("homeTitle") }}</h1>
+    <div v-html="markdownToHtml($t('homeExplanation'))"></div>
+    <!-- <FileLoader v-on:fileLoaded="fileLoaded($event)" /> -->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+// import FileLoader from "@/components/FileLoader.vue";
+// import BranchPositions from "./BranchPositions.vue";
+// import BranchPositions from "./BranchPositions.vue";
+@Component({
+  components: {
+    // FileLoader,
+  },
+  methods: {
+    markdownToHtml(message: string) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const marked = require("marked");
+      return marked(message);
+    },
+    // fileLoaded($event: BranchPositions[]) {
+    //   console.log("Event '" + $event.toString + "' is undefined");
+    //   this.$store.state.branchPositions = $event;
+    // },
+    // fileLoaded($event: BranchPositions[]) {
+    //   console.log("Home level: \n");
+    //   const content = $event;
+    //   // content.forEach((item) => {
+    //   //   console.log(Object.keys(item) + " " + Object.values(item));
+    //   // });
+    //   // const branchPositions = $event;
+    //   this.$emit("fileLoadedAgain", content);
+    // },
+  },
+})
+export default class Home extends Vue {}
+</script>
