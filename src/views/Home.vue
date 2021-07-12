9200707f5add392537b93508db399a14719d1fdd
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-07-12 14:34:06 -0400

    Add file loader

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,8 +10,7 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 // import FileLoader from "@/components/FileLoader.vue";
-// import BranchPositions from "./BranchPositions.vue";
-// import BranchPositions from "./BranchPositions.vue";
+import BranchPositions from "./BranchPositions.vue";
 @Component({
   components: {
     // FileLoader,
@@ -22,9 +21,6 @@ import Component from "vue-class-component";
       const marked = require("marked");
       return marked(message);
     },
-    // fileLoaded($event: BranchPositions[]) {
-    //   console.log("Event '" + $event.toString + "' is undefined");
-    //   this.$store.state.branchPositions = $event;
     // },
     // fileLoaded($event: BranchPositions[]) {
     //   console.log("Home level: \n");
@@ -37,5 +33,15 @@ import Component from "vue-class-component";
     // },
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  fileLoaded($event: BranchPositions[]): void {
+    console.log("Home level: \n");
+    const content = $event;
+    // content.forEach((item) => {
+    //   console.log(Object.keys(item) + " " + Object.values(item));
+    // });
+    // const branchPositions = $event;
+    this.$emit("fileLoadedAgain", content);
+  }
+}
 </script>
