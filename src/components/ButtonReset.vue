177b6652220dd01719221050c80d6bc9bfedaa61
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-07-15 16:23:42 -0400

    Create basic reset button

---
+++ src/components/ButtonReset.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="custom-file" @click="reset()">{{ $t("button.reset") }}</button>
+</template>
+<script lang="ts">
+import { ActionTypes } from "@/store/actions";
+import Vue from "vue";
+export default class Reset extends Vue {
+  reset(): void {
+    this.$store.dispatch(ActionTypes.ResetData, null);
+  }
+}
+</script>
