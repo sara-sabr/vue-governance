884e5849db73727208d1a887a1021a13b26e403e
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-07-15 15:48:26 -0400

    Add link to source code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav" class="nav nav-pills justify-content-center">
-      <router-link to="/" class="nav-link">{{ $t("homeTitle") }}</router-link>
-      <router-link to="/committees" class="nav-link">{{
-        $t("committeesTitle")
-      }}</router-link>
-      <router-link to="/positions" class="nav-link">{{
-        $t("positionsTitle")
-      }}</router-link>
-    </div>
-    <router-view />
+  <div>
+    <main id="app" property="mainContentOfPage">
+      <div id="nav" class="nav nav-pills justify-content-center">
+        <router-link to="/" class="nav-link">{{ $t("homeTitle") }}</router-link>
+        <router-link to="/committees" class="nav-link">{{
+          $t("committeesTitle")
+        }}</router-link>
+        <router-link to="/positions" class="nav-link">{{
+          $t("positionsTitle")
+        }}</router-link>
+      </div>
+      <router-view />
+    </main>
+    <footer>
+      <div class="container">
+        <a href="https://github.com/sara-sabr/vue-governance">{{
+          $t("link.source")
+        }}</a>
+      </div>
+    </footer>
   </div>
 </template>
