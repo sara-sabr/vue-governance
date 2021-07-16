8afe138e85ad4e9ddfbc365b86810085032e9d0d
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-07-15 20:02:47 -0400

    Remove bootstrap-vue

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
-import { BootstrapVue } from "bootstrap-vue";
+
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import i18n from "./i18n";
 
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
-
-Vue.use(BootstrapVue);
 
 new Vue({
   router,
