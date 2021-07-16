a2060a3faa00ae1d098e54deb4dc2a8792c5b736
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-07-15 22:41:52 -0400

    Move nav above main

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
+    <div id="nav" class="nav nav-pills justify-content-center">
+      <router-link to="/" class="nav-link">{{ $t("homeTitle") }}</router-link>
+      <router-link to="/committees" class="nav-link">{{
+        $t("committeesTitle")
+      }}</router-link>
+      <router-link to="/positions" class="nav-link">{{
+        $t("positionsTitle")
+      }}</router-link>
+    </div>
     <main id="app" property="mainContentOfPage">
-      <div id="nav" class="nav nav-pills justify-content-center">
-        <router-link to="/" class="nav-link">{{ $t("homeTitle") }}</router-link>
-        <router-link to="/committees" class="nav-link">{{
-          $t("committeesTitle")
-        }}</router-link>
-        <router-link to="/positions" class="nav-link">{{
-          $t("positionsTitle")
-        }}</router-link>
-      </div>
       <router-view />
     </main>
     <footer>
