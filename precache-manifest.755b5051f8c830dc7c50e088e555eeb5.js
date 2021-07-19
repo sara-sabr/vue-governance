0a92edf5cc72b620e27911e722acd353db5704d4
gcharest <gcharest@users.noreply.github.com>
2021-07-19 18:25:55 +0000

    deploy: 0f90ed10fc37e9938452ac9c57c3250c47ac4286

---
+++ precache-manifest.755b5051f8c830dc7c50e088e555eeb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70f8ed9081193afa26a",
+    "url": "/css/app.bf2d299f.css"
+  },
+  {
+    "revision": "f369d1008f7506dae3db",
+    "url": "/css/chunk-vendors.ce1e57cf.css"
+  },
+  {
+    "revision": "2ac49955c912199a2449dfa3566c2669",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c70f8ed9081193afa26a",
+    "url": "/js/app.2046a065.js"
+  },
+  {
+    "revision": "f369d1008f7506dae3db",
+    "url": "/js/chunk-vendors.a946f6df.js"
+  },
+  {
+    "revision": "7f04d1ae92d7db1f0b1a82c7417b9a07",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
