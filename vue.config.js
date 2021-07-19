97645510332e44548369d59dbadb67061f88e8d6
Guillaume Charest <guillaume.charest@hrsdc-rhdcc.gc.ca>
2021-07-19 18:46:16 -0400

    Fix lint

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,7 @@ module.exports = {
   configureWebpack: {
     devtool: "source-map",
   },
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/vue-governance/"
-      : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-governance/" : "/",
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = "Gouvernance - Governance";
