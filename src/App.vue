ec5d16f6b1750c298aaf04367a8c1724743827e4
Guillaume Charest <guillaume.charest@hrsdc-rhdcc.gc.ca>
2021-07-20 15:55:23 -0400

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,52 @@
 <template>
   <div>
-    <nav id="nav" class="nav nav-pills justify-content-center">
-      <router-link to="/" class="nav-link">{{ $t("homeTitle") }}</router-link>
-      <router-link to="/committees" class="nav-link">{{
-        $t("committeesTitle")
-      }}</router-link>
-      <router-link to="/positions" class="nav-link">{{
-        $t("positionsTitle")
-      }}</router-link>
-      <router-link to="/pathways" class="nav-link">{{
-        $t("pathwaysTitle")
-      }}</router-link>
-      <button class="nav-link" @click="languageToggle()">
-        {{ $t("languageToggle") }}
-      </button>
+    <a class="visually-hidden-focusable" href="#app">{{
+      $t("navigation.skipToMain")
+    }}</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <router-link to="/" class="nav-link">{{
+              $t("navigation.toHome")
+            }}</router-link>
+            <router-link to="/committees" class="nav-link">{{
+              $t("committeesTitle")
+            }}</router-link>
+            <router-link to="/positions" class="nav-link">{{
+              $t("positionsTitle")
+            }}</router-link>
+            <router-link to="/pathways" class="nav-link">{{
+              $t("pathwaysTitle")
+            }}</router-link>
+            <button class="nav-link btn" @click="languageToggle()">
+              {{ $t("navigation.languageToggle") }}
+            </button>
+          </div>
+        </div>
+      </div>
     </nav>
-    <main id="app" property="mainContentOfPage">
+    <main id="app" property="mainContentOfPage" class="mt-3">
       <router-view />
     </main>
     <footer>
       <div class="container">
         <a href="https://github.com/sara-sabr/vue-governance">{{
           $t("link.source")
+        }}</a>
+        <a class="visually-hidden-focusable" href="#app">{{
+          $t("navigation.toTop")
         }}</a>
       </div>
     </footer>
