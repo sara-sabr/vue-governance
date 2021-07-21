33292da014a2321ae18442277933ccd5d117561a
Guillaume Charest <guillaume.charest@hrsdc-rhdcc.gc.ca>
2021-07-20 22:14:21 -0400

    Create new component for listing positions

---
+++ src/components/PositionsList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <table class="table table-striped table-bordered">
+      <caption></caption>
+      <thead>
+        <th scope="col">
+          {{ $t("page.positions.positionsList.table.key") }}
+        </th>
+        <th scope="col">
+          {{ $t("page.positions.positionsList.table.position") }}
+        </th>
+        <th scope="col">
+          {{ $t("page.positions.positionsList.table.classification") }}
+        </th>
+        <th scope="col">
+          {{ $t("page.positions.positionsList.table.reportsTo") }}
+        </th>
+      </thead>
+      <tbody>
+        <tr v-for="position in positions" :key="position.id">
+          <td>
+            {{ position.id }}
+          </td>
+          <td>
+            {{ position.name[lang] }}
+          </td>
+          <td>{{ getClassLevel(position) }}</td>
+          <td>{{ position.reportsTo }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script lang="ts">
+import { Position } from "@/store/state";
+import Vue, { PropType } from "vue";
+import Component from "vue-class-component";
+@Component({
+  props: {
+    positions: {
+      type: Array as PropType<Position[]>,
+      required: true,
+    },
+    lang: {
+      type: String,
+      required: true,
+    },
+  },
+})
+export default class PositionsList extends Vue {
+  getClassLevel(position: Position): string {
+    return position.classification + "-0" + position.level;
+  }
+}
+</script>
