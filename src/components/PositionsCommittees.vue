a74c41cc65fa0ebd67fb784030fb66e2d14a9ae8
Guillaume Charest <guillaume.charest@hrsdc-rhdcc.gc.ca>
2021-07-20 22:14:36 -0400

    Create component to list committees attended by position

---
+++ src/components/PositionsCommittees.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="position in positions" :key="position.id">
+      <h3>{{ position.name[lang] }}</h3>
+      <p>{{ $t("page.positions.positionsCommittees.attends") }}:</p>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Committee, Position } from "@/store/state";
+import Vue, { PropType } from "vue";
+import Component from "vue-class-component";
+@Component({
+  props: {
+    positions: {
+      type: Array as PropType<Position[]>,
+      required: true,
+    },
+    committees: {
+      type: Array as PropType<Committee[]>,
+      required: true,
+    },
+  },
+  methods: {
+    listCommitteesPerPosition(position: Position): void {
+      return;
+    },
+  },
+})
+export default class PositionsCommittees extends Vue {
+  lang = this.$i18n.locale;
+}
+</script>
