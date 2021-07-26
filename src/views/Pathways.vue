4c7764c01f44bb7dca5f61440e9b316a29b5ba83
Guillaume Charest <guillaume.charest@hrsdc-rhdcc.gc.ca>
2021-07-26 10:46:27 -0400

    Move details to new component

--- src/views/Pathways.vue
+++ src/views/Pathways.vue
@@ -2,32 +2,28 @@
   <div class="container">
     <h1 class="text-center">{{ $t("page.pathways.title") }}</h1>
     <p>{{ $t("page.pathways.description") }}</p>
-    <div v-for="pathway in pathways" :key="pathway.id">
-      <h2 v-if="pathway.name[lang].length > 0">
-        {{ $t("page.pathways.pathway") }}: {{ pathway.name[lang] }}
-        {{ pathway.name[lang] }}
-      </h2>
-      <div v-for="(step, stepKey) in pathway.steps" :key="step.id">
-        <h3>
-          {{ $t("page.pathways.step") }} {{ stepKey + 1 }}:
-          {{ step.name[lang] }}
-        </h3>
-        <p>{{ step.description[lang] }}</p>
-      </div>
-      <!-- <p>{{ $tc("message.title") }}</p> -->
-    </div>
+    <pathway-details
+      v-for="pathway in pathways"
+      :key="pathway.id"
+      :pathway="pathway"
+      :lang="lang"
+    ></pathway-details>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
 import { mapState } from "vuex";
+import PathwayDetails from "@/components/PathwayDetails.vue";
 @Component({
   data() {
     return {
       lang: this.$i18n.locale,
       totalTime: 0,
     };
+  },
+  components: {
+    PathwayDetails,
   },
   computed: mapState(["pathways"]),
 
