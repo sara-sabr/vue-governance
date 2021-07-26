1c1e6d6c3cd4b287239df89305ab0d9362bb7522
Guillaume Charest <guillaume.charest@hrsdc-rhdcc.gc.ca>
2021-07-26 15:37:58 -0400

    Add local i18n terms

--- src/components/PathwayDetails.vue
+++ src/components/PathwayDetails.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="pathway-details">
     <h2>{{ $t("page.pathways.pathway") }}: {{ pathway.name[lang] }}</h2>
+
     <div v-for="(step, stepKey) in pathway.steps" :key="step.id">
       <h3>
         {{ $t("page.pathways.step") }} {{ stepKey + 1 }}:
         {{ step.name[lang] }}
       </h3>
       <p>{{ step.description[lang] }}</p>
+      <p>{{ $t("minimumDelay") }}</p>
+      <p>{{ $t("expectedDelay") }}</p>
     </div>
   </div>
 </template>
@@ -23,6 +26,21 @@ import Component from "vue-class-component";
     lang: {
       type: String,
       required: true,
+    },
+  },
+  data() {
+    return { minimumDelay: 0, expectedDelay: 0 };
+  },
+  i18n: {
+    messages: {
+      en: {
+        minimumDelay: "Minimum delay for step's end to end completion: ",
+        expectedDelay: "Expected delay for step's end to end completion: ",
+      },
+      fr: {
+        minimumDelay: "Délai minimum pour compléter l'étape: ",
+        expectedDelay: "Délai attendu pour compléter l'étape: ",
+      },
     },
   },
 })
