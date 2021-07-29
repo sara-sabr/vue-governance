3171b75acdaa3379fcfc106cf4ea4dbd8cf1780b
Guillaume Charest <guillaume.charest@hrsdc-rhdcc.gc.ca>
2021-07-29 14:53:59 -0400

    Add approach accordion to page

--- src/views/Committees.vue
+++ src/views/Committees.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="container">
+    <h1 class="text-center">{{ $t("page.committees.title") }}</h1>
+    <div class="accordion mb-3" id="accordion-committees-approach">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="approach">
+          <button
+            class="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseOne"
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            {{ $t("page.approach") }}
+          </button>
+        </h2>
+        <div
+          id="collapseOne"
+          class="accordion-collapse collapse show"
+          aria-labelledby="approach"
+          data-bs-parent="#accordion-committees-approach"
+        >
+          <div
+            v-html="markdownToHtml($t('page.committees.description'))"
+            class="accordion-body"
+          ></div>
+        </div>
+      </div>
+    </div>
     <CommitteeList
       :rates="rates"
       :committees="committees"
@@ -18,6 +46,13 @@ import { mapState } from "vuex";
   computed: mapState(["rates", "positions", "committees"]),
   components: {
     CommitteeList,
+  },
+  methods: {
+    markdownToHtml(message: string) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const marked = require("marked");
+      return marked(message);
+    },
   },
 })
 export default class Home extends Vue {
