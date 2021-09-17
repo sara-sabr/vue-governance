26c5731f84ff04c979c90ebce8bd64613fee8f4b
Guillaume Charest <guillaume.charest@hrsdc-rhdcc.gc.ca>
2021-09-17 17:34:21 -0400

    Update graph data with computed

--- src/components/CommitteePositionMap.vue
+++ src/components/CommitteePositionMap.vue
@@ -1,4 +1,4 @@
-<template>
+<template ref="map">
   <div class="committee-details accordion mb-3" id="accordion-map">
     <div class="accordion-item">
       <h2 class="accordion-header" id="map">
@@ -20,208 +20,189 @@
         data-bs-parent="#accordion-map"
       >
         <div class="accordion-body">
-          <div id="chart"></div>
+          <button
+            v-if="showGraph === false"
+            class="btn btn-primary"
+            @click="chart(graphData)"
+          >
+            Generate Chart
+          </button>
+          <button
+            v-if="showGraph === true"
+            class="btn btn-primary"
+            @click="deleteChart"
+          >
+            Remove Chart
+          </button>
+          <div id="chart" class="mt-3"></div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { Committee, GraphLink, GraphNode, Graph } from "@/store/state";
+import { GraphLink, GraphNode, Graph } from "@/store/state";
 import { Vue } from "vue-property-decorator";
 import Component from "vue-class-component";
-import { mapGetters, mapState } from "vuex";
+import { mapState } from "vuex";
 import * as d3 from "d3";
 import * as d3Sankey from "d3-sankey";
 
 @Component({
   data() {
-    return { graphData: this.$store.getters.getCommitteesPositionsGraphData };
+    return {
+      // graphData: this.$store.getters.getCommitteesPositionsGraphData,
+      showGraph: false,
+      colorTrue: "#fff",
+      colorFalse: "#000",
+      settings: {
+        margin: { top: 30, right: 30, bottom: 30, left: 30 },
+        height: 500,
+        width: 900,
+        color: "#ccc",
+      },
+    };
   },
   computed: {
-    ...mapGetters(["getCommitteesPositionsGraphData"]),
+    graphData(): Graph {
+      return this.$store.getters.getCommitteesPositionsGraphData;
+    },
     ...mapState(["lang", "committees", "positions"]),
   },
-})
-export default class CommitteePositionMap extends Vue {
-  thisLang = this.$store.state.lang;
-
-  colorTrue = "#fff";
-  colorFalse = "#000";
-  settings = {
-    margin: { top: 30, right: 30, bottom: 30, left: 30 },
-    height: 500,
-    width: 900,
-    color: "#ccc",
-  };
-
-  color = d3.scaleOrdinal(["#d4b4a3"], ["#da4f81"]).unknown("#ccc");
-
-  getAttendees(committee: Committee): string[] {
-    return [
-      ...committee.chairs,
-      ...committee.viceChairs,
-      ...committee.members,
-      ...committee.standingParticipants,
-    ];
-  }
-  /**
-   * References leveraged to combine D3js, D3-Sankey, Vuejs and Typescript
-   * D3js + Typescript: https://gist.github.com/MagicJohnJang/3cde82004e632e66b0fc5c156a7c16e9
-   * Vue + Typescript:
-   * - https://masteringjs.io/tutorials/vue/vue-d3
-   * - https://vuejsexamples.com/tag/d3/
-   * D3-Sankey:
-   * - https://github.com/d3/d3-sankey#readme
-   * - https://www.d3-graph-gallery.com/sankey
-   */
-  /**
-   * TODO:
-   * - Change graph nodes dimensions based on number of nodes of each type
-   * - Test various fonts
-   * - Adjust Nodes spacing
-   * - Change colors (need more since more than 10 nodes)
-   *  */
-
-  chart(graph: Graph): void {
-    // const graph: Graph = this.$store.getters.getCommitteesPositionsGraphData;
-    var width = 960;
-    //Setting height based on number of positions
-    var height = graph.nodes.length * 15;
-    var svg = d3
-      .select("#chart")
-      .append("svg")
-      .attr("viewBox", "0 0 " + width + " " + height);
-
-    var formatNumber = d3.format(",.0f");
-
-    var format = function (d: number | { valueOf(): number }) {
-      return formatNumber(d) + " Committees";
-    };
-
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
-
-    var sankey = d3Sankey
-      .sankey<GraphNode, GraphLink>()
-      .nodeId((d) => d.nodeId);
-
-    sankey
-      .nodeWidth(15)
-      .nodePadding(10)
-      .extent([
-        [1, 1],
-        [width - 1, height - 6],
-      ]);
-
-    var link = svg
-      .append("g")
-      .attr("class", "links")
-      .attr("fill", "none")
-      .attr("stroke", "#000")
-      .attr("stroke-opacity", 0.2)
-      .selectAll<SVGPathElement, GraphLink>("path");
-
-    var node = svg
-      .append("g")
-      .attr("class", "nodes")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .selectAll<SVGGElement, GraphNode>("g");
-
-    sankey(graph);
-
-    link = link
-      .data(graph.links)
-      .enter()
-      .append("path")
-      .attr("d", d3Sankey.sankeyLinkHorizontal())
-      .attr("stroke-width", function (d: GraphLink) {
-        if (d.width) {
-          return Math.max(1, d.width);
-        }
-        return 1;
+  methods: {
+    chart(graph: Graph): void {
+      this.$data.showGraph === true
+        ? (this.$data.showGraph = false)
+        : (this.$data.showGraph = true);
+      // let graph: Graph = this.$store.getters.getCommitteesPositionsGraphData;
+      var width = this.$data.settings.width;
+      //Setting height based on number of positions
+      var height = graph.nodes.length * 15;
+      d3.select("#chart").selectChild("svg").remove();
+      var svg = d3
+        .select("#chart")
+        .append("svg")
+        .attr("viewBox", "0 0 " + width + " " + height);
+      var formatNumber = d3.format(",.0f");
+      var format = function (d: number | { valueOf(): number }) {
+        return formatNumber(d) + " Committees";
+      };
+      var color = d3.scaleOrdinal(d3.schemeCategory10);
+      var sankey = d3Sankey
+        .sankey<GraphNode, GraphLink>()
+        .nodeId((d) => d.nodeId);
+      sankey
+        .nodeWidth(15)
+        .nodePadding(10)
+        .extent([
+          [1, 1],
+          [width - 1, height - 6],
+        ]);
+      var link = svg
+        .append("g")
+        .attr("class", "links")
+        .attr("fill", "none")
+        .attr("stroke", "#000")
+        .attr("stroke-opacity", 0.2)
+        .selectAll<SVGPathElement, GraphLink>("path");
+      var node = svg
+        .append("g")
+        .attr("class", "nodes")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .selectAll<SVGGElement, GraphNode>("g");
+      sankey(graph);
+      link = link
+        .data(graph.links)
+        .enter()
+        .append("path")
+        .attr("d", d3Sankey.sankeyLinkHorizontal())
+        .attr("stroke-width", function (d: GraphLink) {
+          if (d.width) {
+            return Math.max(1, d.width);
+          }
+          return 1;
+        });
+      link.append("title").text(function (d: GraphLink) {
+        return d.source + " → " + d.target + "\n" + format(d.value);
       });
-
-    link.append("title").text(function (d: GraphLink) {
-      return d.source + " → " + d.target + "\n" + format(d.value);
-    });
-
-    node = node.data(graph.nodes).enter().append("g");
-
-    node
-      .append("rect")
-      .attr("x", function (d: GraphNode) {
-        if (d.x0 !== undefined) {
-          return d.x0;
+      node = node.data(graph.nodes).enter().append("g");
+      node
+        .append("rect")
+        .attr("x", function (d: GraphNode) {
+          if (d.x0 !== undefined) {
+            return d.x0;
+          }
+          return 0;
+        })
+        .attr("y", function (d: GraphNode) {
+          if (d.y0 !== undefined) {
+            return d.y0;
+          }
+          return 0;
+        })
+        .attr("height", function (d: GraphNode) {
+          if (d.y0 !== undefined && d.y1 !== undefined) {
+            return d.y1 - d.y0;
+          }
+          return 0;
+        })
+        .attr("width", function (d: GraphNode) {
+          if (d.x1 !== undefined && d.x0 !== undefined) {
+            return d.x1 - d.x0;
+          }
+          return 0;
+        })
+        .attr("fill", function (d: GraphNode) {
+          return color(d.name.replace(/ .*/, ""));
+        })
+        .attr("stroke", "#000");
+      node
+        .append("text")
+        .attr("x", function (d: GraphNode) {
+          if (d.x0 !== undefined) {
+            return d.x0 - 6;
+          }
+          return 0;
+        })
+        .attr("y", function (d: GraphNode) {
+          if (d.y1 !== undefined && d.y0 !== undefined) {
+            return (d.y1 + d.y0) / 2;
+          }
+          return 0;
+        })
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "end")
+        .text(function (d: GraphNode) {
+          return d.name;
+        })
+        .filter(function (d: GraphNode) {
+          if (d.x0 !== undefined) {
+            return d.x0 < width / 2;
+          }
+          return false;
+        })
+        .attr("x", function (d: GraphNode) {
+          if (d.x1 !== undefined) {
+            return d.x1 + 6;
+          }
+          return 0;
+        })
+        .attr("text-anchor", "start");
+      node.append("title").text(function (d: GraphNode) {
+        if (d.value !== undefined) {
+          return d.name + "\n" + format(d.value);
         }
-        return 0;
-      })
-      .attr("y", function (d: GraphNode) {
-        if (d.y0 !== undefined) {
-          return d.y0;
-        }
-        return 0;
-      })
-      .attr("height", function (d: GraphNode) {
-        if (d.y0 !== undefined && d.y1 !== undefined) {
-          return d.y1 - d.y0;
-        }
-        return 0;
-      })
-      .attr("width", function (d: GraphNode) {
-        if (d.x1 !== undefined && d.x0 !== undefined) {
-          return d.x1 - d.x0;
-        }
-        return 0;
-      })
-      .attr("fill", function (d: GraphNode) {
-        return color(d.name.replace(/ .*/, ""));
-      })
-      .attr("stroke", "#000");
-
-    node
-      .append("text")
-      .attr("x", function (d: GraphNode) {
-        if (d.x0 !== undefined) {
-          return d.x0 - 6;
-        }
-        return 0;
-      })
-      .attr("y", function (d: GraphNode) {
-        if (d.y1 !== undefined && d.y0 !== undefined) {
-          return (d.y1 + d.y0) / 2;
-        }
-        return 0;
-      })
-      .attr("dy", "0.35em")
-      .attr("text-anchor", "end")
-      .text(function (d: GraphNode) {
         return d.name;
-      })
-      .filter(function (d: GraphNode) {
-        if (d.x0 !== undefined) {
-          return d.x0 < width / 2;
-        }
-        return false;
-      })
-      .attr("x", function (d: GraphNode) {
-        if (d.x1 !== undefined) {
-          return d.x1 + 6;
-        }
-        return 0;
-      })
-      .attr("text-anchor", "start");
-
-    node.append("title").text(function (d: GraphNode) {
-      if (d.value !== undefined) {
-        return d.name + "\n" + format(d.value);
-      }
-      return d.name;
-    });
-  }
-
-  mounted(): void {
-    this.chart(this.$data.graphData);
-  }
-}
+      });
+    },
+    deleteChart() {
+      d3.select("#chart").selectChild("svg").remove();
+      this.$data.showGraph === true
+        ? (this.$data.showGraph = false)
+        : (this.$data.showGraph = true);
+    },
+  },
+})
+export default class CommitteePositionMap extends Vue {}
 </script>
